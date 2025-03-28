6b011ec915cc2478935fcbb23aaaa6b49c4a4123
Nicolas Duarte <nickboytube0418@gmail.com>
2025-03-28 09:40:57 -0300

    Testando o token de autenticação

--- projeto_jwt/app_jwt/urls.py
+++ projeto_jwt/app_jwt/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('criar/', view=views.registrar, name="registrar"),
     path('logar/', view=views.logar, name="logar"),
+    path('teste/', view=views.view_protegida, name="view_protegida"),
 ]
